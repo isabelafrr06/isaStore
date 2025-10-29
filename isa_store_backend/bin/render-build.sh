27262e6be09ffbb1f07655f159565c84a99ff4e3
Isabela <isabelafrr06@hotmail.com>
2025-10-29 16:10:27 -0600

    render

--- isa_store_backend/bin/render-build.sh
+++ isa_store_backend/bin/render-build.sh
@@ -4,15 +4,12 @@ set -o errexit
 
 # Install dependencies
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+
+# Precompile assets (skip errors if no assets to compile)
+set +e
+bundle exec rails assets:precompile 2>&1
+bundle exec rails assets:clean 2>&1
+set -e
 
 # Run migrations
 bundle exec rails db:migrate
-
-
-
-
-
-
-
